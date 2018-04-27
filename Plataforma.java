d423e805cb5ae1136340a0d9421f6d4f5d749307
Ricardo Vieira <rv.kaki@gmail.com>
2018-04-27 21:30:25 +0100

    pequenas correções em Plataforma

--- Plataforma.java
+++ Plataforma.java
@@ -102,7 +102,8 @@ public class Plataforma{
 
         do{
             nif = ler("NIF");
-        }while(nif.length() != 9 && (nif.charAt(0) != '1' && nif.charAt(0) != '2' && nif.charAt(0) != '5' && nif.charAt(0) != '0'));
+        }while(nif.length() != 9 || (nif.charAt(0) != '1' && nif.charAt(0) != '2' && nif.charAt(0) != '5' && nif.charAt(0) != '0'));
+        //posteriormente remover nif.charAt(0) == '0'
 
         if(this.totalEntidades.containsKey(nif)){
             Entidade e = this.totalEntidades.get(nif);
@@ -235,7 +236,7 @@ public class Plataforma{
 
         do{
             nif = ler("NIF");
-        }while(nif.length() != 9 && (nif.charAt(0) != '1' && nif.charAt(0) != '2' && nif.charAt(0) != '5' && nif.charAt(0) != '0'));
+        }while(nif.length() != 9 || (nif.charAt(0) != '1' && nif.charAt(0) != '2' && nif.charAt(0) != '5' && nif.charAt(0) != '0'));
 
         
         if(! this.totalEntidades.containsKey(nif)){
